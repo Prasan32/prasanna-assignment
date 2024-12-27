7d627ff1f376c483586a3a3accc12868bb2bc7d6
Prasanna Ku. Baniya <prasannakb440@gmail.com>
2024-12-27 19:12:23 +0545

    changed default port to 3000

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -2,7 +2,7 @@ import { config as conf } from 'dotenv';
 conf();
 
 const _config = {
-    PORT: process.env.PORT || 5000,
+    PORT: process.env.PORT || 3000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
 };
